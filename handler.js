41a86f43179c7d79ddd06019f4891a6646419251
Matthew Wigginton Conway <matt@indicatrix.org>
2016-09-16 13:05:39 -0300

    initial commit.

---
+++ handler.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var https = require('https')
+
+// Your first function handler
+module.exports.trevorbot = (event, context, cb) => {
+  if (event.body.text.toLowerCase().indexOf('where') > -1) {
+    if (event.body.text.toLowerCase().replace('trevorbot', '').indexOf('trevor') > -1) {
+      https.get({ host: 'nomadlist.com', path: '/trevorgerhardt.json'}, (res) => {
+        let body = ''
+
+        res.on('data', (d) => body += d)
+        res.on('end', () => {
+          let data = JSON.parse(body)
+          cb(null, { text: `Trevor is in ${data.location.now.city.replace(/[^ a-zA-Z]/g, '?')}, ${data.location.now.country.replace(/[^ a-zA-Z]/g, '?')} (https://nomadlist.com/trevorgerhardt)` })
+        })
+      })
+    } else {
+      cb(null, { text: 'I only know where Trevor is.' })
+    }
+  } else {
+	  cb(null, { text: 'I don\'t understand, I\'m afraid' })
+  }
+}
+
